bfd9266156b32edec4f04c672f6dcbffcf1bc325
Mohammadbasher <m.anwar.basher@gmail.com>
2025-05-14 12:02:56 -0500

    Day 2 practice

--- RDSML-Day-4 👉 R Programming Basics (Part-2)/r Basics (Part-2).R
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/r Basics (Part-2).R
@@ -25,9 +25,40 @@ class(c)
 
 class(a>b)
 print(a<b)
-# < is a relational operator. There are more relational operators
+# < is a relational operator.
+# There are more relational operators such as equal as == , not equal as !=
 x = 19
 y = 22-3
 
 print (x == y)
 print (x != y)
+print(y>x)
+
+
+# Logical operators in R
+# We are comparing a TRUE event with another TRUE,
+# FALSE with TRUE, FALSE with FALSE and TRUE with FALSE
+# && is AND logical operator
+# AND operator shows TRUE only if both events are TRUE
+4 == 4 && 5 == 5
+
+# || is OR logical operator
+# OR operator shows TRUE if one event is TRUE
+x != y || x == y
+
+# Simple Mathematical operators in R
+z = 6 + 9 - 8 * (2 / 3)
+print(z)
+
+w = 4 + 3 - 3 * 2/3
+print(w)
+
+# ^ is power or exponent
+y = 4^3
+print(y)
+
+# %% is for remainder
+# %/% is for resultant without decimal point
+print(11/5)
+print(11%%5)
+print(11%/%5)
