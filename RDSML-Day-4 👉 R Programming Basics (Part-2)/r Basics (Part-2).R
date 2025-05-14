7095abf6e0c2c375c03b93782d200012da2bd533
Mohammadbasher <m.anwar.basher@gmail.com>
2025-05-14 08:06:16 -0500

    R basics Part 2
    Learning more basics

---
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/r Basics (Part-2).R
@@ -0,0 +1,33 @@
+# Data types in R
+# 1. Numeric - Integer/Floating point
+# 2. String/Character - "Mohammad Basher"
+# 3. Logical - 
+
+a = 11
+b = 7.19
+class(a)
+class(b)
+
+
+
+name = 'NBICT'
+print(name)
+class(name)
+
+
+c = "52"
+print(c+9)
+print(a+9)
+
+class(c)
+
+# Logical data types - TRUE,FALSE (T, F)a
+
+class(a>b)
+print(a<b)
+# < is a relational operator. There are more relational operators
+x = 19
+y = 22-3
+
+print (x == y)
+print (x != y)
