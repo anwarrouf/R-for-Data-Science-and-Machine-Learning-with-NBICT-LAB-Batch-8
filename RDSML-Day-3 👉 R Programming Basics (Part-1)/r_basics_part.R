b40d032b68c892795d36409fcadfe8777650578f
Mohammadbasher <m.anwar.basher@gmail.com>
2025-05-13 20:13:44 -0500

    RDSML day 3 codes
    Day 3 codes

---
+++ RDSML-Day-3 👉 R Programming Basics (Part-1)/r_basics_part.R
@@ -0,0 +1,11 @@
+# calculating some health parameters
+my_age = 97
+my_weight = 60
+my_height = 65
+
+x = (my_height*my_age)/my_weight
+print(x)
+print(x,2)
+print(x,digit=4)
+student_heights = c(50,55,64,71,45.25)
+mean(student_heights)
