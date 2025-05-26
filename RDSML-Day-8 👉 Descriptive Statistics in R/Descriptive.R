3ed21344e907e89f4edbf34ea5aba35a2bc6f8d5
Mohammadbasher <m.anwar.basher@gmail.com>
2025-05-26 08:25:07 -0500

    Update Descriptive.R

--- RDSML-Day-8 👉 Descriptive Statistics in R/Descriptive.R
+++ RDSML-Day-8 👉 Descriptive Statistics in R/Descriptive.R
@@ -13,6 +13,25 @@ install.packages("nycflights13")
 library(nycflights13)
 # On the other hand, if we need to use given data of a package, then we use attach()
 attach(nycflights13::flights)
-# Lest look at the first six record using the head() function. Head() shows first 6 rows. If we want to see more rows, then put the number)
+
+# Lest look at the first six records using the head() function. Head() shows first 6 rows. If we want to see more rows, then put the number)
 head(nycflights13::flights)
 
+# If we want to see whole dataset, then we have to do
+nycflights13::flights
+# The above command shows huge data, that's why, we use head function to understand the dataset
+
+# If we want to see last six rows of a data set, then we use tail function
+tail(nycflights13::flights)
+summary(distance)
+# Summary is a base package of R. In psych package, there is a function- describe like summary function, that gives more details
+describe(distance)
+
+# If we want to descriptive analysis of multiple columns, then we need to bundle the desired columns first
+# cbind is a function that create bundle of multiple columns
+
+demo  = cbind(arr_delay,dep_delay, distance)
+demo
+
+# Now we can use describe function
+describe(demo)
