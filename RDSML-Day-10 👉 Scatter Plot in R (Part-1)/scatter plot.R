e6e98f9f66e2f6f95704272689d2689cdf3109e5
Mohammadbasher <m.anwar.basher@gmail.com>
2025-05-31 07:33:37 -0500

    scatter plot part 1

---
+++ RDSML-Day-10 👉 Scatter Plot in R (Part-1)/scatter plot.R
@@ -0,0 +1,24 @@
+# Install and loading ggplot2
+# gg stands for grammer of graph
+install.packages("ggplot2")
+library(ggplot2)
+
+dataset = mtcars
+# mtcars is a dataset from R basics
+
+# Generating a basic scatter plot
+# independent variable in x-axis, dependent variable in y-axis
+ggplot(dataset,aes(x = wt, y = mpg)) +
+  geom_point()
+
+# aes to indicate which data to be used, geom_point is for scatter plot
+# changing the point size and shape
+# Different shape is indicated by different number; for example, 8 is for star
+
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point(size = 2,shape = 8)
+
+# Adding the Regression line
+
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point(size = 2,shape = 8) + 
