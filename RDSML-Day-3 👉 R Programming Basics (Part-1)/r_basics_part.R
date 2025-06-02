36ea613588bbd50d59c564d83493a7d1bf8a0fec
Mohammadbasher <m.anwar.basher@gmail.com>
2025-06-01 23:45:29 -0500

    updated

--- RDSML-Day-3 👉 R Programming Basics (Part-1)/r_basics_part.R
+++ RDSML-Day-3 👉 R Programming Basics (Part-1)/r_basics_part.R
@@ -5,7 +5,13 @@ my_height = 65
 
 x = (my_height*my_age)/my_weight
 print(x)
+
+
 print(x,2)
+
 print(x,digit=4)
+
 student_heights = c(50,55,64,71,45.25)
 mean(student_heights)
+# to get minimum value, maximum value, median, mean, 1st and 3rd quartile, we need to use summary() function
+summary(student_heights)
