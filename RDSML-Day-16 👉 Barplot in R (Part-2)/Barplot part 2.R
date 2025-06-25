74c21bdb39f7dc42c73c3aa720f102d316c51f7d
Mohammadbasher <m.anwar.basher@gmail.com>
2025-06-25 11:20:49 -0500

    Update Barplot part 2.R

--- RDSML-Day-16 👉 Barplot in R (Part-2)/Barplot part 2.R
+++ RDSML-Day-16 👉 Barplot in R (Part-2)/Barplot part 2.R
@@ -20,3 +20,51 @@ ggplot(dataset,aes(x = dose,y = length, color = dose))+
   scale_color_manual(values = c("blue","red","green"))
 
 # We can also use hexadecimal color code instead of using exact name of the colors
+
+# Using brewer color palette. Need to provide name of the color palette
+ggplot(dataset,aes(x = dose,y = length, color = dose))+
+  geom_bar(stat = "identity", fill = "white")+
+  scale_color_brewer(palette = "Dark2")
+
+# Using grey scale
+ggplot(dataset,aes(x = dose,y = length, color = dose))+
+  geom_bar(stat = "identity", fill = "white")+
+  scale_color_grey()
+  
+# Using no background, by choosing theme_classic
+ggplot(dataset,aes(x = dose,y = length, color = dose))+
+  geom_bar(stat = "identity", fill = "white")+
+  scale_color_grey()+
+  theme_classic()
+
+# To change fill colors by groups
+ggplot(dataset,aes(x = dose,y = length, fill = dose))+
+  geom_bar(stat = "identity")
+
+# Using minimal theme to remove background
+
+ggplot(dataset,aes(x = dose,y = length, fill = dose))+
+  geom_bar(stat = "identity")+
+  theme_minimal()
+
+# Using customize bar fill color
+ggplot(dataset,aes(x = dose,y = length, fill = dose))+
+  geom_bar(stat = "identity")+
+  scale_fill_manual(values = c("yellow","purple","pink"))+
+  theme_minimal()
+
+# We can also use brewer color palette like above exampe
+
+# Using black outline color
+ggplot(dataset,aes(x = dose,y = length, fill = dose))+
+  geom_bar(stat = "identity", color = "black")+
+  scale_fill_manual(values = c("blue","pink","white"))
+
+
+# changing legend position. If legend.position = "none", nothing will appear
+ggplot(dataset,aes(x = dose,y = length, fill = dose))+
+  geom_bar(stat = "identity", color = "black")+
+  scale_fill_manual(values = c("blue","pink","white"))+
+  theme(legend.position = "top")
+
+
