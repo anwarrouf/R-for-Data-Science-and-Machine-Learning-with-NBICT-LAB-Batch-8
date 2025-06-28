4b5ff0efb9180e79c283500d12dc9d52232730b0
Mohammadbasher <m.anwar.basher@gmail.com>
2025-06-28 15:34:18 -0500

    Update Bar plot.R

--- RDSML-Day-15 👉 Barplot in R (Part-1)/Bar plot.R
+++ RDSML-Day-15 👉 Barplot in R (Part-1)/Bar plot.R
@@ -1,6 +1,7 @@
 # Creating a data frame
 
-dataset = data.frame(dose = c("D0.5","D1","D2"),length = c(4.2,10,29.5))
+dataset = data.frame(dose = c("D0.5","D1","D2"),
+                     length = c(4.2,10,29.5))
 dataset
 
 library(ggplot2)
@@ -26,4 +27,23 @@ ggplot(dataset,aes(x = dose, y = length))+
 # minimal theme with blue fill color
 ggplot(dataset,aes(x = dose, y = length))+
   geom_bar(stat = "identity", fill = "steelblue",color="yellow")+
+  coord_flip()+
   theme_minimal()
+
+# Bar plot with label that is we want show the values on top or inside of the bar
+ggplot(dataset,aes(x = dose, y = length))+
+  geom_bar(stat = "identity", fill = "steelblue",color="yellow")+
+  geom_text(aes(label=length), vjust = -0.5, size = 3, color = "red")+ # values are shown, vjust i.e. vertical justification is needed
+  theme_minimal()
+# If we give positive number to vjust, then the values are shown inside the bar
+
+myCarData = mtcars
+# Bar plot of counts
+# We want to build a bar plot on the basis of number of cylinders
+ggplot(myCarData, aes(x=factor(cyl)))+
+  geom_bar(stat = "count")+
+  labs(x="number of cylinder")+
+  theme_minimal()
+
+# stat is identity used as the plot is using the exact plot of that plot. On the other hand, stat = count then the plot is built on count
+
