35a7889b5d730e524208f26854298f61237ff4dd
Mohammadbasher <m.anwar.basher@gmail.com>
2025-07-11 22:04:32 -0500

    styling plots

---
+++ RDSML-Day-20 👉 Styling Plots for Publications/Styling plots.R
@@ -0,0 +1,9 @@
+# Styling plots for publications
+# Library required: 
+# Additional resourcs for "Publication-ready scatter plots in R". Search it.
+
+library(ggplot2)
+install.packages("ggthemes")
+library(ggthemes)
+install.packages("viridis")
+library(viridis)
