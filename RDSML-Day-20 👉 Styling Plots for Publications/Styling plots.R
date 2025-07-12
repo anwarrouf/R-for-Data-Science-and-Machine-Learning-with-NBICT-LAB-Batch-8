e28f47afb8b286beef728269a5ac05d2cc5da0cc
Mohammadbasher <m.anwar.basher@gmail.com>
2025-07-12 10:28:41 -0500

    styling plots

--- RDSML-Day-20 👉 Styling Plots for Publications/Styling plots.R
+++ RDSML-Day-20 👉 Styling Plots for Publications/Styling plots.R
@@ -7,3 +7,37 @@ install.packages("ggthemes")
 library(ggthemes)
 install.packages("viridis")
 library(viridis)
+
+df = mtcars
+
+df$cyl = as.factor(df$cyl)
+
+# Styling the plots
+ggplot(df,aes(x = wt, y = mpg, color = cyl)) +
+  geom_point(size = 3)
+
+# Want to write 'cylinder' instead of 'cyl'
+ggplot(df,aes(x = wt, y = mpg, color = cyl)) +
+  geom_point(size = 3) +
+  scale_color_viridis_d(name = "Cylinders") +
+  labs(
+    title = "Fuel Efficiency by Car weight",
+    x = "Weight (1000 lbs)",
+    y = "Miles per Gallon"
+  ) + 
+  theme_classic()
+
+# Controlling the size of font, use different fonts, alignment of title, 
+ggplot(df,aes(x = wt, y = mpg, color = cyl)) +
+  geom_point(size = 3) +
+  scale_color_viridis_d(name = "Cylinders") +
+  labs(
+    title = "Fuel Efficiency by Car weight",
+    x = "Weight (1000 lbs)",
+    y = "Miles per Gallon"
+  ) + 
+  theme_classic(base_size = 12, base_family = "serif") +
+  theme(plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
+        axis.title = element_text(face = "bold", size = 11)
+        ) 
+  
