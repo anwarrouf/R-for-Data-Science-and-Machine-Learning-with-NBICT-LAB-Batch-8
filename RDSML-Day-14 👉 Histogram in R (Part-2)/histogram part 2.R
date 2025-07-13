7607ba66fb95181dc0bfcda891225303cf42f791
Mohammadbasher <m.anwar.basher@gmail.com>
2025-07-13 07:51:08 -0500

    histogram part 2

--- RDSML-Day-14 👉 Histogram in R (Part-2)/histogram part 2.R
+++ RDSML-Day-14 👉 Histogram in R (Part-2)/histogram part 2.R
@@ -7,7 +7,7 @@ library(ggplot2)
 
 # Creating basic histogram
 ggplot(dataset, aes(x = weight)) +
-  geom_histogram(color = 'white', fill = "red") # color indicates boundary color of bar, # fill for inside color
+  geom_histogram(color = 'white', fill = "red", position = "stack") # color indicates boundary color of bar, # fill for inside color
 
 
 # changing histogram plot line colors by groups
@@ -27,4 +27,11 @@ ggplot(dataset, aes(x = weight, fill = sex)) +
   geom_histogram(color = 'white', alpha = 1, position = "dodge") +
   theme(legend.position = "top") # legend position can be top, bottom, right, left
 
-# Adding mean line for male and female group separately
+# Adding mean line in male and female group separately
+# Need plyr package. Plyr helps to calculate mean of individual group
+
+install.packages("plyr")
+library(plyr)
+group_means = ddply(dataset,"sex",summarise, grp.mean = mean(weight))
+group_means
+
