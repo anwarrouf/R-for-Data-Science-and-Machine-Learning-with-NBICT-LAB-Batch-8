60177f1d7197af7f800f616c204fc74a7791fc3e
Mohammadbasher <m.anwar.basher@gmail.com>
2025-07-17 10:05:16 -0500

    central limit theorem

--- RDSML-Day-22 👉 Central Limit Theorem Using R/CLT.R
+++ RDSML-Day-22 👉 Central Limit Theorem Using R/CLT.R
@@ -10,3 +10,68 @@
   # 2. Sample size matters- the larger the sample size, the closer the sample mean distribution will be to normal
   # 3. Mean of the sampling distribution (i.e. mean of all means calculated)- the average of the sample means equals to the population mean
   # 4. Standard deviation of sampling distribution is smaller than the population spread
+
+# Why CLT is important? CLT is the foundation of inferential statistics.
+  # Most statistical tests (t-test, confidence interval, ANOVA)
+
+
+# Uniform distribution
+
+# Normal distribution
+
+# Skewed distribution
+
+# Demonstration of CLT
+# Generate 10,000 times number of uniform distribution (heights of all pillars almost same)
+
+r <- runif(10000) #runif produces random values from >0 to <1
+r
+hist(r)
+mean_r <- mean(r)
+mean_r
+sd_r <- sd(r)
+sd_r
+
+# Sampling of values from the above r data set
+# Draw four random samples from the distribution
+
+samples1 <- sample(r,4) # (sample name, how many values to choose)
+samples1
+samples2 <- sample(r,4)
+samples2
+samples3 <- sample(r,4)
+samples3
+
+
+mean(samples1)
+mean(samples2)
+mean(samples3)
+
+# Creating 'for' loop
+
+cl4 <- mean(sample(r,4)) # assume cl4 is to calculate 4 samples from 10000 values
+cl4
+
+for (i in 1:1000){
+  cl4 <- c(cl4, mean(sample(r,4)))
+} # this loop will run from 1 to 1000
+cl4
+hist(cl4)
+
+cl10 <- mean(sample(r,10)) # assume cl4 is to calculate 10 samples from 10000 values
+cl10
+
+for (i in 1:1000){
+  cl10 <- c(cl10, mean(sample(r,4)))
+} # this loop will run from 1 to 1000
+cl10
+hist(cl10)
+
+cl100 <- mean(sample(r,100)) # assume cl4 is to calculate 100 samples from 10000 values
+cl100
+
+for (i in 1:1000){
+  cl100 <- c(cl100, mean(sample(r,4)))
+} # this loop will run from 1 to 1000
+cl100
+hist_cl100 = hist(cl100)
