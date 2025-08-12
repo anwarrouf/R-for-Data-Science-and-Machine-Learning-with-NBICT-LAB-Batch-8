a1cf22f81caf343958f0d02d1f61c5bc04815654
Mohammadbasher <m.anwar.basher@gmail.com>
2025-08-11 21:06:31 -0500

    poisson distribution

--- RDSML-Day-30 👉 Poisson Probability Distribution/Poisson distribution.R
+++ RDSML-Day-30 👉 Poisson Probability Distribution/Poisson distribution.R
@@ -7,4 +7,13 @@ ppois(q = 3, lambda = 5) - ppois(q = 2, lambda = 5)
 
 # What is the probability to receive exactly 3 calls per hour?
 
-dpois(q = 3, lambda = 5)
+dpois(x = 3, lambda = 5)
+
+
+n <- seq(0:5)
+n <- seq(0,5)
+n
+barplot(dpois (x = n,lambda = 5))
+install.packages("visualize")
+
+library(visualize)
