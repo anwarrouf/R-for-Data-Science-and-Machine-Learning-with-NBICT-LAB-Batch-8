5bd7f00beb6a462ea357f55d3f893c1e064c261c
Mohammadbasher <m.anwar.basher@gmail.com>
2025-09-15 16:12:36 -0500

    Central limit theorem

--- RDSML-Day-22 👉 Central Limit Theorem Using R/CLT.R
+++ RDSML-Day-22 👉 Central Limit Theorem Using R/CLT.R
@@ -1,17 +1,23 @@
-# Assume we have a data set containing 10,000 values i.e. age of a population.
+# Assume we have a data set containing 10,000 values i.e. ages of a population.
+
 # Distribution of values can be normal, uniform or skewed
-# Let's take a sample of 5 random values from the data set and calculate the mean of these 5 values
-# Now taking other sample of 5 random values and calculate the mean of these 5 values
-# Suppose, the process of taking 5 random values repeated for 1000 times
-# Now, a histogram is formulated using these mean values and 
+
+# Let's take a sample (S1) of 5 random values from the data set and calculate the mean (SM1) of these 5 value
+
+# Now taking another sample (S2) of 5 random values and calculate the mean (SM2) of these 5 values
+
+# Suppose, the process of taking 5 random values (S1, S2,...S1000) repeated for 1000 times (SM1, SM2,......SM1000)
+
+# Now, a histogram is formulated using these mean values
 # This histogram will show normal distribution even though the original data was not normally distributed
 # This is called 'Central Limit Theorem (CLT). Key points of CLT are
-  # 1. Population shape doesn't matter
+  # 1. Population shape (can be uniform, normal or skewed) doesn't matter
   # 2. Sample size matters- the larger the sample size, the closer the sample mean distribution will be to normal
   # 3. Mean of the sampling distribution (i.e. mean of all means calculated)- the average of the sample means equals to the population mean
-  # 4. Standard deviation of sampling distribution is smaller than the population spread
+  # 4. Standard deviation of sampling means is smaller than the population's standard deviation
 
-# Why CLT is important? CLT is the foundation of inferential statistics.
+# Why CLT is important? 
+  # CLT is the foundation of inferential statistics.
   # Most statistical tests (t-test, confidence interval, ANOVA)
 
 
@@ -24,7 +30,19 @@
 # Demonstration of CLT
 # Generate 10,000 times number of uniform distribution (heights of all pillars almost same)
 
-r <- runif(10000) #runif produces random values from >0 to <1
+die_faces <- c(0.16,0.17,0.15,0.18,0.19,0.15)
+barplot(die_faces,
+        xlab = "die faces",
+        ylab = "probability",
+        ylim = c(0,0.2),
+        breaks = seq(0,0.2,by = 0.05), # for intervals in y axis
+        abline(h = 20, col = "red", lwd = 2), # adding a horizontal line i.e. intercept in y axis
+        axes = T,
+        main = "probability vs die faces" # main title
+        )
+
+
+r <- runif(10000) # Generate 10,000 random numbers from a uniform distribution between 0 and 1
 r
 hist(r)
 mean_r <- mean(r)
@@ -33,9 +51,10 @@ sd_r <- sd(r)
 sd_r
 
 # Sampling of values from the above r data set
+
 # Draw four random samples from the distribution
 
-samples1 <- sample(r,4) # (sample name, how many values to choose)
+samples1 <- sample(r,4) # (dataset, how many values to choose)
 samples1
 samples2 <- sample(r,4)
 samples2
---
+++ Two-way ANOVA in R (part 1)/two way ANOVA.R
@@ -0,0 +1,12 @@
+# installing packages
+# install.packages(c("tidyverse", "car", "emmeans", "effectsize", "performance", "broom","kableExtra"))
+install.packages("tidyverse")
+install.packages("car")
+install.packages("emmeans")
+install.packages("effectsize")
+install.packages("performance")
+install.packages("broom")
+install.packages("kableExtra")
+
+
+d <- read.csv("cropdata.csv")
