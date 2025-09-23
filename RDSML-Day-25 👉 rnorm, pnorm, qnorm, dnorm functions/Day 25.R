0906f2f1e1eb486e2e2e281ad0710ccefa9ce4e3
Mohammadbasher <m.anwar.basher@gmail.com>
2025-09-23 16:15:37 -0500

    Day 2

--- RDSML-Day-25 👉 rnorm, pnorm, qnorm, dnorm functions/Day 25.R
+++ RDSML-Day-25 👉 rnorm, pnorm, qnorm, dnorm functions/Day 25.R
@@ -1,12 +1,15 @@
 # rnorm, here, 'r' stands for random and 'norm' stands for normal distribution.
 # In normal distribution, mean should be 0 (near 0) and standard deviation should be 1 (near 1)
-# The more values we have, the more close the mean and sd will be
+# The more values we have, the more close the mean and standard deviation will be
 
-x <- rnorm(10)
+# rnorm generates random numbers for normal or Gaussian distribution (with mean = 0 and sd = 1) and here 10 indicates, we ask for 10 values
+x <- rnorm(10) 
+x
 mean(x)
 sd(x)
-y
+
 y <- rnorm(100)
+y
 mean(y)
 sd(y)
 
@@ -21,13 +24,26 @@ mean(p)
 sd(p)
 
 
-y <- rnorm(10, mean = 100, sd = 5) # we want 10 values that will have mean near 100 and sd near 5)
-mean(y)
-sd(y)
+a <- rnorm(10, mean = 100, sd = 5) # we want 10 values that will have mean near 100 and sd near 5)
+a
+mean(a)
+sd(a)
 
-z <- rnorm(100, mean = 100, sd = 3)
-z
-mean(z)
-sd(z)
+b <- rnorm(100, mean = 100, sd =3)
+b
+mean(b)
+sd(b)
+# For any types of distribution, if we want to calculate probability, then we first need to convert that distribution to standard normal distribution
 
-# pnorm
+
+# pnorm is used to determine the probability by providing z value
+# At z = 0, the value of probability is 0.5
+
+pnorm(0)
+pnorm(-1.96) # in fact,±1.96SD, 95% values will cover. But for conveniences, we say it as ±2SD.
+# Thus, pnorm(-1.96) will give probability value of 0.02499 or 2.499%
+# Thus, 2.5% values will fall outside of either side; in total 2.5% x 2 = 5%. From here, 95% confidence interval comes
+pnorm(-1.64) # that is, in case of z value -1.64, 5% value is outside
+
+
+# qnorm gives z value if probability value is provided. Probability values are between 0 and 1
