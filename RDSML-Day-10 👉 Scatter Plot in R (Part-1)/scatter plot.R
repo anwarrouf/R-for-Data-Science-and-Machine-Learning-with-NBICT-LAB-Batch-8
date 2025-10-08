a622524bbca915aea5a1ff0b163bae833fd699fc
Mohammadbasher <m.anwar.basher@gmail.com>
2025-10-08 00:14:57 -0500

    updated

--- RDSML-Day-10 👉 Scatter Plot in R (Part-1)/scatter plot.R
+++ RDSML-Day-10 👉 Scatter Plot in R (Part-1)/scatter plot.R
@@ -12,13 +12,16 @@ ggplot(dataset,aes(x = wt, y = mpg)) +
   geom_point()
 
 # aes to indicate which data to be used, geom_point is for scatter plot
-# changing the point size and shape
+# changing the point size (default size = 1) and shape
 # Different shape is indicated by different number; for example, 8 is for star
 
 ggplot(dataset, aes(x = wt, y = mpg)) +
   geom_point(size = 2,shape = 8)
 
-# Adding the Regression line
+# Adding the Regression line i.e. best fit line
 
 ggplot(dataset, aes(x = wt, y = mpg)) +
-  geom_point(size = 2,shape = 8) + 
+  geom_point(size = 2,shape = 7) +
+  geom_smooth(method = lm) # shaded area indicates confidence interval
+  
+  
